2b623817916b00cf8fe37349f4c7aa12c861c4eb
Emanuel Eduardo <112293589+emanusousa@users.noreply.github.com>
2026-05-05 09:52:44 -0300

    Teste inicial do uso de sensor de aproximação

--- Project-ReciLuz/firmware/src/main.cpp
+++ Project-ReciLuz/firmware/src/main.cpp
@@ -1,23 +1,75 @@
 #include <Arduino.h>
 
-#define LED_PIN 5      // D5
-#define BUTTON_PIN 4   // D4
+#define LED_PIN 4        
+#define BUTTON_PIN 19    
+
+#define TRIG_PIN 25      
+#define ECHO_PIN 26      
+
+#define PWM_CHANNEL 0
+#define PWM_FREQ 5000
+#define PWM_RESOLUTION 8 
+
+float medirDistancia() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duracao = pulseIn(ECHO_PIN, HIGH, 30000);
+
+  if (duracao == 0) {
+    return -1;
+  }
+
+  float distancia = duracao * 0.034 / 2;
+  return distancia;
+}
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(115200);
+
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 
-  digitalWrite(LED_PIN, LOW); // LED começa apagado
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  ledcSetup(PWM_CHANNEL, PWM_FREQ, PWM_RESOLUTION);
+  ledcAttachPin(LED_PIN, PWM_CHANNEL);
+
+  ledcWrite(PWM_CHANNEL, 0);
 }
 
 void loop() {
   int estadoBotao = digitalRead(BUTTON_PIN);
+  float distancia = medirDistancia();
 
-  if (estadoBotao == LOW) {
-    // botão pressionado
-    digitalWrite(LED_PIN, HIGH);
+  bool botaoPressionado = estadoBotao == LOW;
+
+  int brilho = 0;
+
+  if (distancia > 0 && distancia <= 40) {
+    
+    brilho = map(distancia, 40, 5, 5, 255);
+    brilho = constrain(brilho, 5, 255);
+
+    brilho = brilho * brilho / 255;
   } else {
-    // botão solto
-    digitalWrite(LED_PIN, LOW);
+    brilho = 0;
   }
+
+  if (botaoPressionado) {
+    brilho = 255;
+  }
+
+  ledcWrite(PWM_CHANNEL, brilho);
+
+  Serial.print("Distancia: ");
+  Serial.print(distancia);
+  Serial.print(" cm | Brilho: ");
+  Serial.println(brilho);
+
+  delay(80);
 }
