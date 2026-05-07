d5c9bdc0e4a85dc0c1265ef56455f5ba8cb38a1c
Emanuel Eduardo <112293589+emanusousa@users.noreply.github.com>
2026-05-07 09:39:32 -0300

    Adição do Serial para visualizar informações

--- Project-ReciLuz/firmware/src/main.cpp
+++ Project-ReciLuz/firmware/src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 
-#define LED_PIN 4        
-#define BUTTON_PIN 19    
+#define LED_PIN 4        // LED no GPIO 4
+#define BUTTON_PIN 19    // Botão no GPIO 19
 
-#define TRIG_PIN 25      
-#define ECHO_PIN 26      
+#define TRIG_PIN 25      // Trig do sensor ultrassônico
+#define ECHO_PIN 26      // Echo do sensor ultrassônico
 
 #define PWM_CHANNEL 0
-#define PWM_FREQ 5000
-#define PWM_RESOLUTION 8 
+#define PWM_FREQ 1000
+#define PWM_RESOLUTION 8 // brilho de 0 a 255
 
 float medirDistancia() {
   digitalWrite(TRIG_PIN, LOW);
@@ -40,36 +40,51 @@ void setup() {
   ledcAttachPin(LED_PIN, PWM_CHANNEL);
 
   ledcWrite(PWM_CHANNEL, 0);
+
+  Serial.println("ReciLuz iniciado...");
 }
 
 void loop() {
-  int estadoBotao = digitalRead(BUTTON_PIN);
   float distancia = medirDistancia();
-
-  bool botaoPressionado = estadoBotao == LOW;
+  bool botaoPressionado = digitalRead(BUTTON_PIN) == LOW;
 
   int brilho = 0;
-
-  if (distancia > 0 && distancia <= 40) {
-    
-    brilho = map(distancia, 40, 5, 5, 255);
-    brilho = constrain(brilho, 5, 255);
-
-    brilho = brilho * brilho / 255;
-  } else {
-    brilho = 0;
-  }
+  String modo = "";
 
   if (botaoPressionado) {
     brilho = 255;
+    modo = "MODO MANUAL";
+  } 
+  else if (distancia > 0 && distancia <= 40) {
+
+    brilho = map(distancia, 40, 5, 10, 255);
+    brilho = constrain(brilho, 10, 255);
+
+    brilho = brilho * brilho / 255;
+
+    modo = "NOITE ATIVO / PRESENCA DETECTADA";
+  } 
+  else {
+    brilho = 5; 
+    modo = "NOITE ECONOMICO / SEM PRESENCA";
   }
 
   ledcWrite(PWM_CHANNEL, brilho);
 
+  Serial.println("-------------------------");
   Serial.print("Distancia: ");
   Serial.print(distancia);
-  Serial.print(" cm | Brilho: ");
-  Serial.println(brilho);
+  Serial.println(" cm");
 
-  delay(80);
+  Serial.print("Botao: ");
+  Serial.println(botaoPressionado ? "pressionado" : "solto");
+
+  Serial.print("Brilho: ");
+  Serial.print(brilho);
+  Serial.println(" / 255");
+
+  Serial.print("Modo: ");
+  Serial.println(modo);
+
+  delay(300);
 }
